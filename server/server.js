13e5b9a3806334088bb693eaebcf5ac8e7a92a93
shubhamgiri9548 <ankushgiri2007@gmail.com>
2025-08-13 21:07:35 +0530

    remove slash

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 // Middleware
 const allowedOrigins = [
   "http://localhost:5173", // for Vite local dev
-  "https://visitor-log-system.vercel.app/" // deployed frontend 
+  "https://visitor-log-system.vercel.app" // deployed frontend 
 ];
 
 app.use(cors({
